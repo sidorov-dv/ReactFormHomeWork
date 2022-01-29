6c82d37569c2b537628477c179a8d9f36e5069b9
Dmitry Sidorov <demsys14@gmail.com>
2022-01-29 21:06:26 +0300

    ReactFormValiate

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray } from '@angular/forms';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Form';
+
+  formValid = {};
+
+  profileForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    age: new FormControl('', [Validators.required, Validators.max(99)]),
+    jobs: new FormArray([])
+  });
+
+  onSubmit() {
+    console.log(this.profileForm.value);
+    this.formValid = this.profileForm.value;
+  }
+
+  get jobs() {
+    return this.profileForm.get('jobs') as FormArray;
+  }
+
+  addJob() {
+    this.jobs.push(new FormControl(''));
+  }
+
+  get firstName() {
+    return this.profileForm.get('firstName') as FormControl;
+  }
+
+  get lastName() {
+    return this.profileForm.get('lastName') as FormControl;
+  }
+
+  get age() {
+    return this.profileForm.get('age') as FormControl;
+  }
 }
